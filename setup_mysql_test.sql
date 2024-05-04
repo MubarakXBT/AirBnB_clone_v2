2698eb430307052ae4949efe63404e735b5ecf7a
MubarakXBT <kazeemmubarak@gmail.com>
2024-05-04 14:48:03 +0100

    Task 3-4: script to setup MYSQL server

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,3 +1,4 @@
+-- Prepares a MySQL server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd'
 GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_test_db'@'localhost';
