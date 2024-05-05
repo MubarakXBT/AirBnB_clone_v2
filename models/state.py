5e30a302cdd8b9ddb65408b7697479bb8ab84652
MubarakXBT <kazeemmubarak@gmail.com>
2024-05-05 05:46:55 +0100

    update to place, amenities, base_model, state and city with integration of sql_alchemy

--- models/state.py
+++ models/state.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import shlex
+from sqlalchemy import Column, Integer, String
 import models
 from models.city import City
+import shlex
+
 
 class State(BaseModel, Base):
-    """ State class """
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade='all, delete, delete-orphan',
