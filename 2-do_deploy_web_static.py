a54257c02d29d7285141ba1ed948a2b7a2f1c4d8
MubarakXBT <Kazeemmubarak@gmail.com>
2024-05-31 15:08:32 +0100

    deploy static file to server using fabrics

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+"""
+from os import path
+from fabric.api import *
+from datettime import datetime
+
+
+env.hosts = ['107.22.144.189', '52.3.241.179']
+env.user = ubuntu
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_deploy(archive_path):
+    """
+    """
+    try:
+        if not (path.exists(archive_path)):
+                return False
+
+        # upload archive
+        put(archive_path, /tmp/)
+
+        # getting time_stamp
+        timestamp = archive_path[-18:-4]
+        run('sudo mkdir -p /data/web_static/releases/web_static_{}/'
+            .format(timestamp))
+
+        # Decompress archive
+        run('sudo tar-xzf /tmp/web_static_{}.tgz -C \
+                /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+
+        # delete archive
+        run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
+
+        # delete exisiting symbolic link
+        run('sudo rm -rf /data/web_static/current/')
+        
+        # create new symbolic link
+        run('sudo ln -s /data/web_static/releases/web_static_{}/ /data/web_static/current'.format(timestamp))
+
+    except:
+        return False
+
+    # where Sucess
+    return True
