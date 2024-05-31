ab170f0890b478e8875f2f9363c5f78e2a19478f
MubarakXBT <Kazeemmubarak@gmail.com>
2024-05-31 14:27:00 +0100

    pep8 fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,11 +11,12 @@ def do_pack():
     """
     pack file into tgz archive
     """
+
     filename = strftime("%Y%m%d%H%M%S")
 
     try:
         local("mkdir -p versions")
-        local ("tar -czvf versions/web_static_{}.tgz web_static/"
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
                .format(filename))
 
         return "versions/web_static_{}.tgz".format(filename)
