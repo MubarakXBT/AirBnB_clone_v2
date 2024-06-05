ca023321d19f00a6abde3070a60f8e0090192ac3
MubarakXBT <Kazeemmubarak@gmail.com>
2024-06-05 17:13:55 +0100

    3-update deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# Fabfile to create and distribute an archive to a web server.
 import os.path
 from datetime import datetime
 from fabric.api import env
@@ -7,7 +6,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["107.22.144.189", "52.3.241.179"]
+env.hosts = ['107.22.144.189', '52.3.241.179']
 
 
 def do_pack():
