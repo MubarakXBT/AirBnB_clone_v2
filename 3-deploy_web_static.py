7bd722f335e167cf10f79d1d35603a648d166598
MubarakXBT <Kazeemmubarak@gmail.com>
2024-06-05 20:48:42 +0100

    1

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,10 +2,12 @@
 # Fabfile to create and distribute an archive to a web server.
 import os.path
 from datetime import datetime
-from fabric.api import env, local, put, run, abort
+from fabric.api import env
+from fabric.api import local
+from fabric.api import put
+from fabric.api import run
 
 env.hosts = ["107.22.144.189", "52.3.241.179"]
-
 
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
@@ -25,47 +27,46 @@ def do_pack():
         print(f"Error creating archive: {e}")
         return None
 
-
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
+    """Deploy web files to server
     """
-    if not os.path.isfile(archive_path):
-        print(f"Archive path {archive_path} does not exist.")
-        return False
-
     try:
+        if os.path.isfile(archive_path) is False:
+            return False
         file = archive_path.split("/")[-1]
         name = file.split(".")[0]
 
-        put(archive_path, "/tmp/{}".format(file))
-        run("rm -rf /data/web_static/releases/{}/".format(name))
-        run("mkdir -p /data/web_static/releases/{}/".format(name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name))
-        run("rm /tmp/{}".format(file))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
-        print("New version deployed!")
-        return True
+        # upload archive
+        put(archive_path, '/tmp/{}'.format(file))
+
+        # create target dir
+        run('sudo mkdir -p /data/web_static/releases/{}'.format(name))
+
+        # uncompress archive and delete .tgz
+        run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(file, name))
+
+        # remove archive
+        run('sudo rm /tmp/{}'.format(file))
+
+        # move contents into host web_static
+        run('sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(name, name))
+
+        # remove extraneous web_static dir
+        run('sudo rm -rf /data/web_static/releases/{}/web_static'.format(name))
+
+        # delete pre-existing sym link
+        run('sudo rm -rf /data/web_static/current')
+
+        # re-establish symbolic link
+        run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(file))
     except Exception as e:
-        print(f"Error deploying archive: {e}")
         return False
 
+    # return True on success
+    return True
 
 def deploy():
     """Create and distribute an archive to a web server."""
     file = do_pack()
-    if file is None:
-        print("Failed to create archive.")
-        return False
     result = do_deploy(file)
-    if not result:
-        print("Failed to deploy archive.")
     return result
